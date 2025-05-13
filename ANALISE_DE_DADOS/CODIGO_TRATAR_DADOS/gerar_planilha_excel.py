4ca998938f8f3efbeb2ab6a93bc3ac0c17411e91
mateus-sousa-silva <mateussilvaestudos2712@gmail.com>
2025-05-13 10:26:16 -0300

    Update gerar_planilha_excel.py

--- ANALISE_DE_DADOS/CODIGO_TRATAR_DADOS/gerar_planilha_excel.py
+++ ANALISE_DE_DADOS/CODIGO_TRATAR_DADOS/gerar_planilha_excel.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 nome_planilha = ""
 
-df = pd.read_csv(f"D:\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\BASE_DE_DADOS\{nome_planilha}.csv", sep=',', encoding='utf-8')
+df = pd.read_csv(f".\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\BASE_DE_DADOS\{nome_planilha}.csv", sep=',', encoding='utf-8')
 
-df.to_excel(f"D:\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\PLANILHAS_TRATADAS\{nome_planilha}.xlsx", index=False)
+df.to_excel(f".\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\PLANILHAS_TRATADAS\{nome_planilha}.xlsx", index=False)
 
-df_excel = pd.read_excel(f"D:\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\PLANILHAS_TRATADAS\{nome_planilha}.xlsx")
+df_excel = pd.read_excel(f".\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\PLANILHAS_TRATADAS\{nome_planilha}.xlsx")
 
 print(df_excel)
