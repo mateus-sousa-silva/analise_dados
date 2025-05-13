ccc6b3e26515caf381477d542b65c04f2cade45d
mateus-sousa-silva <mateussilvaestudos2712@gmail.com>
2025-05-13 10:25:21 -0300

    Update gerar_planilha_excel.py
    usando uma variável para conseguir puxar e gerar o nome de uma planilha de forma mais intuitiva

--- ANALISE_DE_DADOS/CODIGO_TRATAR_DADOS/gerar_planilha_excel.py
+++ ANALISE_DE_DADOS/CODIGO_TRATAR_DADOS/gerar_planilha_excel.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("D:\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\BASE_DE_DADOS\despesas_predio_2025.csv", sep=',', encoding='utf-8')
+nome_planilha = ""
 
-df.to_excel("D:\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\PLANILHAS_TRATADAS\despesas_predio_2025.xlsx", index=False)
+df = pd.read_csv(f"D:\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\BASE_DE_DADOS\{nome_planilha}.csv", sep=',', encoding='utf-8')
 
-df_excel = pd.read_excel("D:\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\PLANILHAS_TRATADAS\despesas_predio_2025.xlsx")
+df.to_excel("D:\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\PLANILHAS_TRATADAS\{nome_planilha}.xlsx", index=False)
+
+df_excel = pd.read_excel("D:\PROGRAMACAO_FACULDADE\PROGRAMACAO_DE_COMPUTADORES\ANALISE_DE_DADOS\PLANILHAS_TRATADAS\{nome_planilha}.xlsx")
 
 print(df_excel)
